c157cecf33596b7347fa7c3d2088d77f55ba8963
Benjamin Symons <benjaminsymons@hotmail.com>
2020-07-29 10:05:21 +0800

    Change anchor to Link

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ListErrors from './ListErrors';
 import agent from '../agent';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const mapStateToProps = (state) => ({ ...state.auth });
 
@@ -35,7 +36,7 @@ class Login extends React.Component {
             <div className="col-md-6 offset-md-3 col-xs-12">
               <h1 className="text-xs-center">Sign In</h1>
               <p className="text-xs-center">
-                <a>Need an account?</a>
+                <Link to="/register">Need an account?</Link>
               </p>
 
               <ListErrors errors={this.props.errors} />
